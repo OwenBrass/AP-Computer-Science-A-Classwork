a0d59d6002fb6c9d8c334cc655bdf99018b2d97b
OwenBrass <55504720+OwenBrass@users.noreply.github.com>
2020-02-12 09:26:47 -0800

    Update TumorDetector.java

--- TumorDetector.java
+++ TumorDetector.java
@@ -9,25 +9,20 @@ public class TumorDetector
 {
     public static boolean arrayMatch(int[][] a, int[][] b){
         boolean same=false;
-        for(int n=0; n<4; n++){
-            int[][] c = new int[a.length][a[0].length];
-            c=arrayRotateCC(a);  
           for(int i=0; i<a.length; i++){
             for(int j=0; j<a[0].length; j++){
-                if (c[i][j]!=b[i][j]){
+                if (a[i][j]!=b[i][j]){
                     same = false;
                     i=a.length;
                     j=a[0].length;
+                    //System.out.println("Check1");
                 }
                 else{
                     same=true;
+                    //System.out.println("Check2");
                 }
             }
         }
-        if(same = true){
-            n=4;
-        }
-    }
         return same;
     }
     public static int[][] arrayRotateCC(int[][] a){
@@ -41,16 +36,25 @@ public class TumorDetector
     }
     public static String detectTumor(int[][] scan, int[][] tumor){
         String report = "";
-        for(int i=0; i<scan.length-tumor.length; i++){
-            for(int j=0; j<scan[0].length-tumor[0].length; j++){
-                int[][] scanPart = new int[tumor.length][tumor[0].length];
+        int[][] scanPart = new int[tumor.length][tumor[0].length];
+        for(int i=0; i<scan.length-tumor.length+1; i++){
+            for(int j=0; j<scan[0].length-tumor[0].length+1; j++){
                 for(int p=0; p<tumor.length; p++){
                     for(int q=0; q<tumor[0].length; q++){
                         scanPart[p][q]=scan[i+p][j+q];
                     }
                 }
-                if(arrayMatch(scanPart,tumor)==true){
-                    report += "\nPossible tumor at ("+i+","+j+")";
+                if(arrayMatch(tumor,scanPart)==true){
+                    report += "\nPossible tumor at ("+i+","+j+", 0)";
+                }
+                else if(arrayMatch(tumor,arrayRotateCC(scanPart))==true){
+                    report += "\nPossible tumor at ("+i+","+j+", 270)";
+                }
+                else if(arrayMatch(tumor,arrayRotateCC(arrayRotateCC(scanPart)))==true){
+                    report += "\nPossible tumor at ("+i+","+j+", 180)";
+                }
+                else if(arrayMatch(tumor,arrayRotateCC(arrayRotateCC(arrayRotateCC(scanPart))))==true){
+                    report += "\nPossible tumor at ("+i+","+j+", 90)";
                 }
             }
         }
@@ -61,5 +65,9 @@ public class TumorDetector
         {1,1,0,0,0,0,0,1,0,0},{0,1,0,1,0,0,1,1,1,1},{1,1,1,1,1,0,0,1,1,0},{0,0,0,0,0,0,0,0,0,0},{1,1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1,1}};
         int[][] tum1 = {{0,0,1,1},{0,0,1,0},{0,1,1,1},{0,0,1,1}};
         System.out.println(detectTumor(patient1,tum1));
+        int[][] patient2 = {{0,0,0,0,1,0},{0,0,0,0,1,1},{1,1,1,0,1,0},{0,1,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0}};
+        int[][] tum2 = {{0,1,0},{0,1,1},{0,1,0}};
+        System.out.println(detectTumor(patient2,tum2));
     }
 }
+
